7712c4cd3d3cb6eda60cb1b36fd7263c4787a54e
n0n0man <nschwarz@student.42.fr>
2017-11-24 14:09:17 +0100

    Update haha.sh

--- haha.sh
+++ haha.sh
@@ -1,4 +1,4 @@
 brew install fortune
 brew install cowsay
 brew install lolcat
-while true; do fortune | cowsay | lolcat; sleep 0.000000001; done
+while true; do fortune | cowsay | lolcat; sleep 0.0000001; done
